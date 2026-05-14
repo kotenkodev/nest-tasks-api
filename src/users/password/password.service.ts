6efce7fd6caf76b8568ee075cada99016f90613f
Vadym <vadym.kotenko.dev@gmail.com>
2026-05-14 15:11:52 +0300

    feat: implement password hashing in PasswordService

--- src/users/password/password.service.ts
+++ src/users/password/password.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class PasswordService {}
+export class PasswordService {
+  private readonly SALT_ROUNDS = 10;
+
+  private async hash(password: string): Promise<string> {
+    return await bcrypt.hash(password, this.SALT_ROUNDS);
+  }
+
+  private async verify(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return await bcrypt.compare(password, hashedPassword);
+  }
+}
