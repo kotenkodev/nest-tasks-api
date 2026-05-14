530a19f3d1c85502af51a743aa2854f30032019f
Vadym <vadym.kotenko.dev@gmail.com>
2026-05-14 14:08:28 +0300

    feat: add authentication dependencies and configuration

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -1,10 +1,12 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppConfig } from './app.config';
 import * as Joi from 'joi';
+import { AuthConfig } from './auth.config';
 
 export interface ConfigType {
   app: AppConfig;
   database: TypeOrmModuleOptions;
+  auth: AuthConfig;
 }
 
 export const appConfigSchema = Joi.object({
@@ -15,4 +17,6 @@ export const appConfigSchema = Joi.object({
   DB_PASSWORD: Joi.string().required().default('password'),
   DB_NAME: Joi.string().required().default('tasks'),
   DB_SYNC: Joi.boolean().required().default(false),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRES_IN: Joi.string().required().default('1h'),
 });
