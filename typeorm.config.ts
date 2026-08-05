30ccad6cb35ceffaca9b4e7f080aab54f5d43e70
Vadym <vadym.kotenko.dev@gmail.com>
2026-08-05 10:44:57 +0300

    fix: update environment names in deployment workflows to dev and prod

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -9,8 +9,11 @@ export default new DataSource({
   port: parseInt(process.env.DB_PORT ?? '5432'),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME ?? process.env.DB_DATABASE,
+  database: process.env.DB_NAME,
+
   synchronize: false,
   entities: ['dist/**/*.entity.{js,ts}'],
   migrations: ['dist/src/migrations/**/*.{js,ts}'],
 });
+
+
